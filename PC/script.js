1f1cb9984655bb0f2902344da1362f0b7bbac2ba
Takahata-syu1 <takahata.syu1@gmail.com>
2023-07-27 13:42:52 +0900

    no message

--- PC/script.js
+++ PC/script.js
@@ -2,6 +2,7 @@ $(".mainslide-items").slick({
     autoplay: false,
     dots: false,
     centerMode: true,
+    infinite: true,
     centerPadding: '0px',
     slidesToShow: 1,
     prevArrow: '<img src="./assets/top_img/slick-arrow-left.svg" class="slide-arrow prev-arrow">',
